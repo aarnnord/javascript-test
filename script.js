10396ec4512f6208d907b2c0476dbf27b00c6d63
aarnnord <60853220+aarnnord@users.noreply.github.com>
2020-02-24 13:33:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var elems =document.getElementsByTagName("input");
+
+for(var i=0;i<elems.length;i++)
+{
+    if(elems[i].type=="submit" && elems[i].name =="Submit")
+    {
+        elems[i].click();        
+        break;
+    }
+}
